1bd3dc6928839b5644acc3a39fee3589636607b8
Akshita <mathur_akshita@yahoo.ca>
2016-02-09 18:13:11 -0500

    Commit of what is working correctly so far
    - Everything else is commented out at the moment

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.controller('MathController', function($scope) {
       $scope.gross = 0;
       $scope.insurance = 8;
       $scope.cpp = 11;
-      $scope.gender = ['Female' , 'Male'];
-      $scope.dependants = ['2', '3', '4'];
-      $scope.bonus = 250;
+      // $scope.gender = ['Female' , 'Male'];
+      // $scope.dependants = ['2', '3', '4'];
+      // $scope.bonus = 250;
 
       $scope.empInsurance = function() {
 
@@ -25,19 +25,13 @@ app.controller('MathController', function($scope) {
 
       // }
 
-    $scope.chkVal2 = 'Female';
-		$scope.gender = function(){
-		 alert($scope.chkVal1 + ' ' + $scope.chkVal2);
-     };
+  //   $scope.chkVal2 = 'Female';
+		// $scope.gender = function(){
+		//  alert($scope.chkVal1 + ' ' + $scope.chkVal2);
+  //    };
 
       
 
 
       
 });
-// return ( Number($scope.Gross) - ( Number($scope.Gross) * (Number($scope.IT)/100 ) +  
-// 	Number($scope.Gross) * (Number($scope.CPP)/100 )  + Number($scope.Gross) * (Number($scope.EI)/100 )) );
-// .controller('ExampleController', ['$scope', function($scope) {
-//   $scope.items = ['settings', 'home', 'other'];
-//   $scope.selection = $scope.items[0];
-// }]);
