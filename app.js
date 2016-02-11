b8c27fb8d991e6f164c1c5400d01c610f3dc879b
Akshita <mathur_akshita@yahoo.ca>
2016-02-11 12:25:09 -0500

    Working

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.controller('MathController', function($scope) {
       $scope.selectedG = $scope.genders[0];
       $scope.dependants = ['2', '3', '4'];
       $scope.selectedD = $scope.dependants[0];
-      $scope.bonus = 250;
+      $scope.bonus = 0;
 
       $scope.empInsurance = function() {
 
@@ -28,68 +28,36 @@ app.controller('MathController', function($scope) {
       $scope.bonusAdd = function(){
         return $scope.bonus;
       }
-
-      // $scope.totalSoFar = function(){
-      //   return (Number($scope.gross)  * (Number($scope.insurance)/100)) - (Number($scope.gross) * (Number($scope.cpp)/100)) + (Number($scope.bonus));
-      // }
-
-      // $scope.SwitchFunction = function(selectedG, selectedD){
-      //   var $scope.percentage;
-      //   if(selectedG = "Female"){
-      //     switch(selectedD){
-      //       case 2: $scope.percentage = 0.16;
-      //         break;
-      //       case 3: $scope.percentage = 0.15;
-      //         break;
-      //       case 4: $scope.percentage = 0.14;
-      //         break;
-      //   }else if(selectedG = "Male"){
-      //     switch(selectedD){
-      //       case 2: $scope.percentage = 0.18;
-      //         break;
-      //       case 3: $scope.percentage = 0.17;
-      //         break;
-      //       case 4: $scope.percentage = 0.16;
-      //         break;
-      //   }
-      // }
-      // var $scope.income = ($scope.gross * $scope.percentage); 
-      //   return $scope.income;
-
-      // $scope.maletwo = function(){
-      //   return (Number($scope.gross) * (0.18));
-      // }
-      // $scope.malethree = function(){
-      //   return (Number($scope.gross) * (0.17));
-      // }
-      // $scope.malefour = function(){
-      //   return (Number($scope.gross) * (0.17));
-      // }
-
-  // $scope.calculateTotal = function() {
-  //         var percentage;
+         
+      $scope.incomeTax = function() {
+          $scope.percentage = 0;
           
-  //         if ($scope.selectedG == "F") {
-  //             switch($scope.selectedD) {
-  //                     case 2: percentage = 0.16;
-  //                       break;
-  //                     case 3: percentage = 0.15;
-  //                       break;
-  //                     case 4: percentage = 0.14;
-  //                       break;
-  //             }
-  //         } else {
-  //             switch($scope.selectedD) {
-  //                     case 2: percentage = 0.18;
-  //                       break;
-  //                     case 3: percentage = 0.17;
-  //                       break;
-  //                     case 4: percentage = 0.16;
-  //                       break;
-  //             }
-  //         }
+          if ($scope.selectedG == "Female") {
+              switch($scope.selectedD) {
+                      case "2": $scope.percentage = 0.16;
+                        break;
+                      case "3": $scope.percentage = 0.15;
+                        break;
+                      case "4": $scope.percentage = 0.14;
+                        break;
+              }
+          } else {
+              switch($scope.selectedD) {
+                      case "2": $scope.percentage = 0.18;
+                        break;
+                      case "3": $scope.percentage = 0.17;
+                        break;
+                      case "4": $scope.percentage = 0.16;
+                        break;
+              }
+          }
           
-  //         return (Number($scope.gross) * Number(percentage));
-  //     }
+          return (Number($scope.gross) * Number($scope.percentage));
+        }
+
+        $scope.totalSalary = function(){
+          return (Number($scope.gross) - $scope.empInsurance() - $scope.cppDeduction() - $scope.incomeTax() + Number($scope.bonus));
+        }
+
         
 });
